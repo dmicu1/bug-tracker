6de30bd928cf92fc794899e284301722c96424ca
dmicu1 <danielmicu26@gmail.com>
2026-08-18 11:13:53 +0200

    changing perf threshold

--- tests-perf/script.js
+++ tests-perf/script.js
@@ -1,12 +1,12 @@
+import { check, sleep } from "k6";
 import http from "k6/http";
-import { sleep, check } from "k6";
 
 export const options = {
   duration: "30s",
   vus: 1,
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
-    http_req_duration: ["p(95)<500"], // 95 percent of response times must be below 500ms
+    http_req_duration: ["p(99)<5"], // 95 percent of response times must be below 500ms
   },
 };
 
